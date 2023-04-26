469185c0a5e6cdbf2dfc03be894dc0c0a67a3169
ehud <ehudmi12@gmail.com>
2023-04-26 10:09:20 +0300

    prep deploy

--- deploy_test/server/index.js
+++ deploy_test/server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
-// const { runDB } = require("./config/atlas");
+const path = require("path");
 
 const app = express();
 
@@ -13,10 +13,14 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
 app.use("/users", require("./routes/user.routes"));
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`server is running on port ${process.env.PORT || 8080}`);
 });
-
-// runDB();
